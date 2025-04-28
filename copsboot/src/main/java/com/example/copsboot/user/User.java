8839d318e453a0c4c39621b176a478afd234edc2
Carlos Carmona <zs22017034@estudiantes.uv.mx>
2025-04-27 21:31:01 -0600

    Add files via upload

--- copsboot/src/main/java/com/example/copsboot/user/User.java
+++ copsboot/src/main/java/com/example/copsboot/user/User.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.example.copsboot.user;
 
 import java.util.Set;
@@ -32,3 +33,39 @@ public class User {
         return roles;
     }
 }
+=======
+package com.example.copsboot.user;
+
+import java.util.Set;
+import java.util.UUID;
+
+public class User {
+    private UUID id;
+    private String email;
+    private String password;
+    private Set<UserRole> roles;
+
+    public User(UUID id, String email, String password, Set<UserRole> roles) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Set<UserRole> getRoles() {
+        return roles;
+    }
+}
+>>>>>>> 15a875795c521f90a90bc333929174567ad64411
--- copsboot/src/main/java/com/example/copsboot/user/UserRole.java
+++ copsboot/src/main/java/com/example/copsboot/user/UserRole.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.example.copsboot.user;
 
 public enum UserRole {
@@ -5,3 +6,12 @@ public enum UserRole {
     CAPTAIN,
     ADMIN
 }
+=======
+package com.example.copsboot.user;
+
+public enum UserRole {
+    OFFICER,
+    CAPTAIN,
+    ADMIN
+}
+>>>>>>> 15a875795c521f90a90bc333929174567ad64411
